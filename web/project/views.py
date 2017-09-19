28ecb16b46fd605dd71ba757fee4f613e7f09e7a
Lucas Barros <lucas.lima@pop-rn.rnp.br>
2017-09-19 11:15:35 -0300

    Utilizando blocos de html que se repetem. Paginas e rotas basicas funcionais. Servir como exemplo de uso

--- web/project/views.py
+++ web/project/views.py
@@ -4,12 +4,58 @@ from project.app import app
 from project.models import *
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/index')
 def index():
-    if request.method == 'POST':
-        text = request.form['text']
-        post = Post(text)
-        db.session.add(post)
-        db.session.commit()
-    posts = Post.query.order_by(Post.date_posted.desc()).all()
-    return render_template('index.html', posts=posts)
+    return render_template('index.html')
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/blank')
+def blank():
+    return render_template('blank.html')
+
+@app.route('/buttons')
+def buttons():
+    return render_template('buttons.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/flot')
+def flot():
+    return render_template('flot.html')
+
+@app.route('/morris')
+def morris():
+    return render_template('morris.html')
+
+@app.route('/forms')
+def forms():
+    return render_template('forms.html')
+
+@app.route('/notifications')
+def notifications():
+    return render_template('notifications.html')
+
+@app.route('/grid')
+def grid():
+    return render_template('grid.html')
+
+@app.route('/panels-wells')
+def panels_wells():
+    return render_template('panels-wells.html')
+
+@app.route('/tables')
+def tables():
+    return render_template('tables.html')
+
+@app.route('/icons')
+def icons():
+    return render_template('icons.html')
+
+@app.route('/typography')
+def typography():
+    return render_template('typography.html')
